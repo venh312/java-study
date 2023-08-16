81ba50a7ef94aedcea94aa702c73bc6b8e56fce8
venh <conf312@naver.com>
2023-08-16 18:40:26 +0900

    [이펙티브 자바] ITEM 15 - 클래스와 멤버의 접근 권한을 최소화 하라.

--- src/main/java/effective/item15/Main.java
+++ src/main/java/effective/item15/Main.java
@@ -13,16 +13,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AGE : " + Age.AGE);
 
-        print(Age.AGE_ARR);
-        Age.AGE_ARR[0] = 99;
-        Age.AGE_ARR[1] = 99;
-        Age.AGE_ARR[2] = 99;
-        print(Age.AGE_ARR);
+//        print(Age.AGE_ARR);
+//        Age.AGE_ARR[0] = 99;
+//        Age.AGE_ARR[1] = 99;
+//        Age.AGE_ARR[2] = 99;
+//        print(Age.AGE_ARR);
 
-        int[] ageClone = Age.AGE_ARR.clone();
-        ageClone[0] = 77;
-        ageClone[1] = 77;
-        ageClone[2] = 77;
+        int[] ageClone = Age.values();
         print(ageClone);
     }
 }
