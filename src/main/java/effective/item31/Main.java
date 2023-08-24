3989d520c46f351bbb2fd121cbeae6c7e46967f8
conf312 <conf312@naver.com>
2023-08-25 07:40:57 +0900

    Feature/mac (#7)
    * [이펙티브 자바] ITEM 26 - 로 타입은 사용하지 말라.
    
    * [이펙티브 자바] ITEM 26 - 로 타입은 사용하지 말라.
    
    * [이펙티브 자바] ITEM 27 - 비검사 경고를 제거하라.
    
    * [이펙티브 자바] ITEM 27 - 비검사 경고를 제거하라.
    
    * [이펙티브 자바] ITEM 28 - 배열보다는 리스트를 사용하라.
    
    * [이펙티브 자바] ITEM 29 - 이왕이면 제네릭 타입으로 만들라.
    
    * [이펙티브 자바] ITEM 30 - 이왕이면 제네릭 메서드로 만들라.
    
    * [이펙티브 자바] ITEM 31 - 한정적 와일드카드를 사용해 API 유연성을 높이자.
    
    * [이펙티브 자바] ITEM 31 - 한정적 와일드카드를 사용해 API 유연성을 높이자.
    
    ---------
    
    Co-authored-by: VenhMac <gpfm312@gmail.com>

--- src/main/java/effective/item31/Main.java
+++ src/main/java/effective/item31/Main.java
@@ -7,8 +7,13 @@ public class Main {
         StackGeneric<Number> stackGeneric = new StackGeneric<>();
         Iterable<Integer> integers = new ArrayList<>();
         // 매개변수화 타입이 불공변이기 때문에 오류 메시지가 뜬다.
-        stackGeneric.pushAll(integers);
-
+        //stackGeneric.pushAll(integers);
+      
+        /*
+            해결책 - 한정적 와일드카드 타입
+            E의 Iterable 이 아니라 E의 하위 타입의 Iterable 이어야 한다.
+            Iterable<? extends E>
+        */
         //stackGeneric.pushAllWildcard(integers);
     }
 }
