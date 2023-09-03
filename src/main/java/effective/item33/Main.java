94f34c96b237ed81ccf63d6a053e0469e4ffae40
VenhMac <gpfm312@gmail.com>
2023-09-04 07:39:47 +0900

    [이펙티브 자바] ITEM 33 - 타입 안전 이종 컨테이너를 고려하라.

--- src/main/java/effective/item33/Main.java
+++ src/main/java/effective/item33/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 // 코드 33-1
 class Favorites {
-    private Map<Class<?>, Object> favorites = new HashMap<>();
+    private final Map<Class<?>, Object> favorites = new HashMap<>();
 
     public <T> void putFavorite(Class<T> type, T instance){
         // favorites 가 비한정적 와일드카드 타입이라 이 맵 안에 아무것도 넣을수 없다고 생각되지만,
@@ -15,6 +15,7 @@ class Favorites {
     } // 클래스의 리터럴 타입은 Class가 아니라 Class<T>이다.
 
 
+    // 동적 형변환으로 런타임 타입 안정성 확보 (로 타입으로 넘어오는 타입 안전성이 깨지는걸 방지한다.)
     public <T> void putFavoriteDynamic(Class<T> type, T instance){
         favorites.put(Objects.requireNonNull(type), type.cast(instance));
     }
@@ -27,15 +28,12 @@ class Favorites {
 }
 
 public class Main {
+
     public static void main(String[] args) {
         Favorites f = new Favorites();
         f.putFavorite(String.class, "JAVA");
         f.putFavorite(Integer.class, 0xcafebabe);
         f.putFavorite(Class.class, Favorites.class);
-
-        List list = new ArrayList<>();
-        f.putFavorite(List.class, list);
-        f.getFavorite(List.class);
 
         String favoriteString = f.getFavorite(String.class);
         int favoriteInteger = f.getFavorite(Integer.class);
