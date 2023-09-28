2e7127f91baf8e9e8b31c93bee4c3aadd582065a
conf312 <conf312@naver.com>
2023-09-28 11:14:53 +0900

    Feature/mac (#32)
    [이펙티브 자바] ITEM 64 - 객체는 인터페이스를 사용해 참조하라.

---
+++ src/main/java/effective/item64/Main.java
@@ -0,0 +1,12 @@
+package effective.item64;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] intArr = {1,2,3,4,5};
+        List<Integer> list = new ArrayList<>(Arrays.asList(intArr));
+    }
+}
