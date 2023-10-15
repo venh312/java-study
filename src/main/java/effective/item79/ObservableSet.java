595d4fb02b441b12c79a68f9c205002302b6e852
VenhMac <gpfm312@gmail.com>
2023-10-16 07:45:01 +0900

    [이펙티브 자바] ITEM 79 - 과도한 동기화는 피하라. #4

--- src/main/java/effective/item79/ObservableSet.java
+++ src/main/java/effective/item79/ObservableSet.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ObservableSet<E> extends ForwardingSet<E> {
     public ObservableSet(Set<E> s) {
@@ -11,6 +12,7 @@ public class ObservableSet<E> extends ForwardingSet<E> {
     }
 
     private final List<SetObserver<E>> observers = new ArrayList<>();
+    //private final List<SetObserver<E>> observers = new CopyOnWriteArrayList<>();
 
     public void addObserver(SetObserver<E> observer) {
         synchronized (observers) {
@@ -25,17 +27,17 @@ public class ObservableSet<E> extends ForwardingSet<E> {
     }
 
     private void notifyElementAdded(E element) {
-//        synchronized (observers) {
-//            for (SetObserver<E> observer : observers) {
-//                observer.added(this, element);
-//            }
-//        }
-        List<SetObserver<E>> snapshot = null;
+        synchronized (observers) {
+            for (SetObserver<E> observer : observers) {
+                observer.added(this, element);
+            }
+        }
+        /*List<SetObserver<E>> snapshot = null;
         synchronized(observers) {
             snapshot = new ArrayList<>(observers);
         }
         for (SetObserver<E> observer : snapshot)
-            observer.added(this, element);
+            observer.added(this, element);*/
     }
 
     @Override
